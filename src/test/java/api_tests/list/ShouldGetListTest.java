fedc5b4cede6bef9111c4849d1b3836b52221b5f
pSuvorov <svrv.pvl@gmail.com>
2022-04-18 17:44:54 +0300

    Made get list test repeated 3 times

--- src/test/java/api_tests/list/ShouldGetListTest.java
+++ src/test/java/api_tests/list/ShouldGetListTest.java
@@ -4,24 +4,24 @@ import api_tests.ListBaseTest;
 import io.restassured.response.Response;
 import model.list.GetListResponseBody;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShouldGetListTest extends ListBaseTest {
     private static String listId;
-    private static GetListResponseBody getListResponseBody;
     private static final Boolean EXPECTED_CLOSED_VALUE = false;
 
     @BeforeAll
     public static void arrange(){
         listId = createDefaultListAndReturnId(BOARD_ID);
-        Response getListResponse = listClient.sendListGetRequest(listId);
-        getListResponseBody = getListResponse.as(GetListResponseBody.class);
     }
 
-    @Test
+    @RepeatedTest(3)
     public void act(){
+        Response getListResponse = listClient.sendListGetRequest(listId);
+        GetListResponseBody getListResponseBody = getListResponse.as(GetListResponseBody.class);
         assertEquals(listId, getListResponseBody.getId());
         assertEquals(BOARD_ID, getListResponseBody.getIdBoard());
         assertEquals(EXPECTED_CLOSED_VALUE, getListResponseBody.getClosed());
