65287366ed5e0a1c7879601e446623df30204458
pSuvorov <svrv.pvl@gmail.com>
2022-04-19 13:13:15 +0300

    Added 404 check on update list

---
+++ src/test/java/api_tests/list/ShouldGetErrorWhenUpdateListWhichDoesNotExist.java
@@ -0,0 +1,33 @@
+package api_tests.list;
+
+import api_tests.ListBaseTest;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import rest_clients.ListClient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShouldGetErrorWhenUpdateListWhichDoesNotExist extends ListBaseTest {
+    private static String boardId;
+
+    @BeforeAll
+    public static void arrange(){
+        boardId = createDefaultBoardAndReturnId();
+    }
+
+    @Test
+    public void act(){
+        listClient = new ListClient();
+        String notExistingListId = "6245ceb79f1e246fb742c211";
+        Response response = listClient.sendUpdateListRequest(notExistingListId,"name", "newName");
+        assertEquals(404, response.statusCode());
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        deleteBoard(boardId);
+    }
+
+}
