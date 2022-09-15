8a9e95ff3ca08d788969067839cf7403df38c8fb
svrv-pvl <svrv.pvl@gmail.com>
2022-09-15 16:15:04 +0300

    Update RestClient.java
    Added TODO to remove token

--- src/test/java/rest_clients/RestClient.java
+++ src/test/java/rest_clients/RestClient.java
@@ -10,6 +10,7 @@ public class RestClient {
     protected final String BASE_URL = "https://api.trello.com/1/";
     protected final String KEY = "999d1bdf19e2a37ef3ad781a42b456fb";
     protected final String TOKEN = "d254d2de41b17424e1d964c40a86448481f5acad0a5b0ff651d590605f80f83a";
+    //TODO Временное решение с хранением токена для отладки. Необходимо перенести в ресурс вне гита
 
     protected RequestSpecification prepareRequest(){
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
